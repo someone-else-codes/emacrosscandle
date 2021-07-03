6affed07a1612b158e2d4a021e082208c6781773
Someone Else <kc42@gmail.com>
2021-07-03 14:04:48 -0700

    form downloads data

--- web-ui/main.js
+++ web-ui/main.js
@@ -1,4 +1,29 @@
 
+function process_data(t) {
+	write_output(t);
+}
+
+function load_data(u) {
+//	alert(u);
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", u, true);
+	xhr.onload = function (e) {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				process_data(xhr.responseText);
+			} else {
+				process_error(xhr.statusText);
+			}
+		}
+	};
+	xhr.onerror = function (e) { process_error(xhr.statusText); };
+	xhr.send(null); 
+}
+
+function build_data_url (d) {
+	return 'https://api.coingecko.com/api/v3/coins/' + d["asset"] + '/market_chart?vs_currency=' + d["currency"] + '&days=7&interval=hourly';
+}
+
 function parse_form() {
 	var f = document.forms["input"];
 	return {
@@ -7,6 +32,7 @@ function parse_form() {
 		timeframe: f["timeframe"].value,
 		units: f["units"].value,
 	};
+
 }
 
 function write_output(t) {
@@ -22,5 +48,7 @@ function clicked_form() {
 	o += d["timeframe"] + " " + d["units"] + "\n";
 
 	write_output(o);
+
+	load_data(build_data_url(d));
 
 }
