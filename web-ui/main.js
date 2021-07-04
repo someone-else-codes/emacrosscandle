5a350bdb725f278358d21c2d7746d33446f097bd
Someone Else <kc42@gmail.com>
2021-07-04 15:51:55 -0700

    showing emas

--- web-ui/main.js
+++ web-ui/main.js
@@ -1,8 +1,16 @@
 
-function show_data(a) {
+function show_data(d) {
+	write_output("emas: " + JSON.stringify(d.emas) + "\n");
+	var a = d.prices;
 	for (var i = 0; i < a.length; ++i) {
-		var d = a[i];
-		write_output(d.date.toISOString() + "\t" + d.price + "\n");
+		var p = a[i];
+		write_output(p.date.toISOString() + "\t" + p.price + "\n");
+		var s = '';
+		for (var j = 0; j < d.emas.length; ++j) {
+			s = s + "\t" + d.emas[j] + ": " + p["ema-" + d.emas[j]];
+		}
+		s = s + "\n";
+		write_output(s);
 	}
 }
 
@@ -15,27 +23,31 @@ function process_price(a) {
 	return o;
 }
 
-function process_data(t) {
+function process_data(o, t) {
 	var d = JSON.parse(t); 
 
 	//write_output(t);
 
 	var a = d["prices"] // price data should be here as 2 - element arrays of timestamp/price pairs
-	var o = new Array();
+	var p = new Array();
 	for (var i = 0; i < a.length; ++i) {
-		o.push(process_price(a[i]));
+		p.push(process_price(a[i]));
 	}
+	o.prices = p;
+	calculate_emas(o);
+	o.prices.sort(function (a,b) { return b.date - a.date });
 	return o;
 }
 
-function load_data(u) {
+function load_data(d) {
+	var u = build_data_url(d)
 //	alert(u);
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", u, true);
 	xhr.onload = function (e) {
 		if (xhr.readyState === 4) {
 			if (xhr.status === 200) {
-				show_data(process_data(xhr.responseText));
+				show_data(process_data(d, xhr.responseText));
 			} else {
 				process_error(xhr.statusText);
 			}
@@ -49,6 +61,23 @@ function build_data_url (d) {
 	return 'https://api.coingecko.com/api/v3/coins/' + d["asset"] + '/market_chart?vs_currency=' + d["currency"] + '&days=' + d["days"] + '&interval=' + d["interval"];
 }
 
+function string_to_numbers(s) {
+	var a = new Array();
+	var n = s.split(/\s+/);
+	for (i = 0; i < n.length; ++i) {
+		var p = parseInt(n[i]);
+		if (! isNaN(p)) { a.push(p); }
+	}
+	return a;
+}
+
+function parse_emas(t) {
+	var a = string_to_numbers(t);
+	a.sort(function (a,b) { return b - a; });
+	// we want to sort in reverse numerical order
+	// because we'll be checking for crossings against the largest ema
+	return a;
+}
 function parse_form() {
 	var f = document.forms["input"];
 	var o = {
@@ -57,9 +86,44 @@ function parse_form() {
 		interval: f["interval"].value,
 	};
 	o.days = (('daily' == o.interval) ? 330 : 14);
+	o.emas = parse_emas(f["emas"].value);
 	return o;
 	
 
+}
+
+function calculate_ema(p, e) {
+	var k = "ema-" + e;
+	write_output("calculating " + k + "\n");
+	//to calculate the ema, we must calculate the SMA for the first value.
+	// then the ema for each period is:
+	// (current price - previous ema) * smoothing constant + previous ema
+	// the smoothing constant is 2 / (ema level + 1)
+	var s = 2 / (e + 1);
+	//first we need to calculate the sma for the first value.
+	// we'll do this by averaging the first n values where n is the ema level
+	var ema = 0;
+	for (var i = 0; i < p.length; ++i) {
+		if (i < e - 1) {
+			// we're still accumulating data for the first ema
+			ema += p[i].price;
+		} else if (i == e - 1) {
+			ema = ema / e;
+			p[i][k] = ema;
+		} else {
+			ema = (p[i].price - ema) * s + ema;
+			p[i][k] = ema;
+		}
+	}
+
+}
+
+function calculate_emas(d) {
+	// this function modifies d
+	write_output("calculating emas\n");
+	for (var i = 0; i < d.emas.length; ++i) {
+		calculate_ema(d.prices, d.emas[i]);
+	}
 }
 
 function write_output(t) {
@@ -75,6 +139,6 @@ function clicked_form() {
 
 	write_output(o);
 
-	load_data(build_data_url(d));
+	load_data(d);
 
 }
